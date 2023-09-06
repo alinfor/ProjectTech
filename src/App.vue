bf76313bdc4c52afc305a53274c343652914e355
Alifor <116239087+alinfor@users.noreply.github.com>
2023-09-06 08:21:30 +0200

    Header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
 import HelloWorld from './components/HelloWorld.vue'
 import MoviesList from './components/MoviesList.vue';
 </script>
 
 <template>
+  <Header/>
   <HelloWorld msg="Vite + Vue" />
   <MoviesList/>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header className="bg-[#003141] py-4">
+    <div className="container mx-auto flex justify-between items-center">
+      <div>
+        <img src="../assets/logo.jpg" class="h-13 w-20">
+      </div>
+    </div>
+  </header>
+</template>
